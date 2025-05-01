dacb858d983f3069af20abde7741240850863807
17070610 <schimea@hotmail.com>
2025-05-01 12:55:12 +0200

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,8 @@
--- Creates a trigger that updates item quantity after order insertion
-DROP TRIGGER IF EXISTS after_order_insert;
+-- Creates a trigger that decreases the quantity
+-- of an item after adding a new order.
+DROP TRIGGER IF EXISTS reduce_quantity;
 DELIMITER $$
+CREATE TRIGGER reduce_quantity
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
@@ -8,4 +10,4 @@ BEGIN
         SET quantity = quantity - NEW.number
         WHERE name = NEW.item_name;
 END $$
-DELIMITER;
+DELIMITER ;
