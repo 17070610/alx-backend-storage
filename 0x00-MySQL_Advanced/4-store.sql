f987070042adfefaeedab15922286c073c5eaf31
17070610 <schimea@hotmail.com>
2025-05-01 12:51:18 +0200

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,14 +1,11 @@
 -- Creates a trigger that updates item quantity after order insertion
-
-DELIMITER//
-
 CREATE TRIGGER after_order_insert
+DELIMITER $$
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = quantity - NEW.number
-    WHERE id = NEW.item_id;
-END//
-
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
 DELIMITER;
